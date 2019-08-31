54d55bdcb7482f43b01ea9e47e1b6a785eddac2f
Lylist <1303404574@qq.com>
2019-08-31 17:39:51 +0800

    Add files via upload
    create

---
+++ data_analyze/analysis.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+class analyzer:
+    def __init__(self, point):
+        self.point = point
+
+    def sort(self):
+        pass
+
+    def process(self):
+        self.sort()
+
+
+if __name__ == '__main__':
+    print('1')
---
+++ handler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import getopt
+
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    input_file = ''
+    file_type = 'csv'
+    try:
+        opts, args = getopt.getopt(argv, "hi:t:", ["ifile=", "filetype="])
+    except getopt.GetoptError:
+        print('-i <inputfile> -t <csv or pcap (default is csv)>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print("-i <inputfile> -t <csv or pcap (default is csv)>")
+            sys.exit()
+        elif opt in ('-i', '--ifile'):
+            input_file = arg
+        elif opt in ('-t', '--filetype'):
+            if arg not in ('csv', 'pacp'):
+                print('file_type is wrong. Please use -h!')
+                sys.exit(2)
+            file_type = arg
+
+    print('file_type:%s' % file_type)
+    print('input_file:%s' % input_file)
